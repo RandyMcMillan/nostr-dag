ac58964412dbcc0555c4e01e4986d9eda76e069e
randymcmillan <randymcmillan@protonmail.com>
2026-08-23 16:59:23 -0400

    src/bin/keygen.rs:more log levels

--- src/bin/keygen.rs
+++ src/bin/keygen.rs
@@ -4,18 +4,29 @@
 //! and shows the `cargo run --bin federation` commands for each daemon.
 
 use nostr::Keys;
+use tracing::{debug, info, trace};
 
 fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::from_default_env()
+                .add_directive("keygen=info".parse().unwrap()),
+        )
+        .init();
+
+    info!("starting federation key generation");
     println!("Generating 5 federation keys...\n");
 
     let mut pubkeys = Vec::new();
     let mut secrets = Vec::new();
 
     for i in 1..=5 {
+        trace!(member = i, "generating keypair");
         let keys = Keys::generate();
         let sk_hex = keys.secret_key().to_secret_hex();
         let pk_hex = keys.public_key().to_hex();
 
+        debug!(member = i, pubkey = %pk_hex, "generated keypair");
         println!("=== Federation Member {} ===", i);
         println!("Secret key: {}", sk_hex);
         println!("Public key: {}", pk_hex);
@@ -26,6 +37,7 @@ fn main() {
     }
 
     let all_pubkeys = pubkeys.join(",");
+    info!(member_count = pubkeys.len(), "finished federation key generation");
 
     println!("=== Configuration ===\n");
     println!("FEDERATION_PUBKEYS={}\n", all_pubkeys);
